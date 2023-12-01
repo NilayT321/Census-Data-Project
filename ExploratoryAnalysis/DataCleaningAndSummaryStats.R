cb244a6db925e344e2183327cea1dc0977323e49
HaoYangGuan2024 <152650600+HaoYangGuan2024@users.noreply.github.com>
2023-12-01 18:13:48 -0500

    Update DataCleaningAndSummaryStats.R

--- ExploratoryAnalysis/DataCleaningAndSummaryStats.R
+++ ExploratoryAnalysis/DataCleaningAndSummaryStats.R
@@ -4,10 +4,13 @@ adultvars <- c("age", "workclass", "fnlwgt", "education", "educationnum",
                "martialstatus", "occupation", "relationship", "race", "sex", 
                "capitalgain", "capitalloss", "hoursperweek", "nativecountry", "50k")
 
-rawadult <- read.csv("adult.data", header = FALSE, col.names = adultvars , skip = 1)
+adult <- read.csv("adult.data", header = FALSE, col.names = adultvars , skip = 1)
 
-adult <- subset(rawadult, select = -c(capitalgain, capitalloss, fnlwgt))
+#Remove unwanted variable:
+adult <- subset(adult, select = -c(capitalgain, capitalloss, fnlwgt,
+                                    race, nativecountry, education, relationship))
 
+#sex and 50k binary:
 #Female = 1, Male = 2
 adult$sex <- as.factor(adult$sex)
 adult$sex<- as.numeric(adult$sex) 
@@ -16,9 +19,18 @@ adult$sex<- as.numeric(adult$sex)
 adult$X50k <- as.factor(adult$X50k)
 adult$X50k <- as.numeric(adult$X50k)
 
-adult = adult %>% 
-  filter(occupation != " ?" & workclass != " ?" & nativecountry != " ?") 
+#Re-code workclass
+adult <- adult %>%
+  mutate(workclass = ifelse(workclass %in% 
+                      c(" Federal-gov", " Local-gov", " State-gov"), 
+                      " gov", workclass))
 
-# Some people are volunteers (workclass = Without-pay)
+#workclass, occupation filtering:
+adult <- adult %>% 
+  filter(!(workclass %in% c(" ?", " Without-pay", " Never-worked")))
 
+adult <- adult %>%
+  filter(!(occupation %in% c(" ?")))
+
+#Add 3 to educationnum
 adult$educationnum <- adult$educationnum + 3
