88aed27ac8af106c4f7889681d7c66ba9787d754
NilayT <ntripathi2003@gmail.com>
2023-12-06 11:53:24 -0500

    Get a preliminary SVM model

--- PredictiveModeling/SVM.R
+++ PredictiveModeling/SVM.R
@@ -1,51 +1,67 @@
 library(dplyr)
 library(e1071)
 
+set.seed(2)
 adult = read.csv("Adult_NoOutlier.csv")
 adult = subset(adult, select = -c(X))
 
-adult$X50k = as.factor(adult$X50k)
+# Get the raw adult data
+raw_adult = read.csv("adult.data")
+
+# Recode martial status 
+# Married-AF-spuse and Married-civ-spuse and Married-spouse absent -> Married 
+# Divorced, never-married, separated, widowed
+adult$maritalstatus = case_when(
+  adult$maritalstatus %in% c(" Married-civ-spouse", " Married-AF-spouse", "Married-spouse-absent") ~ "Married",
+  adult$maritalstatus %in% c(" Divorced", " Separated") ~ "Divorced/Separated",
+  adult$maritalstatus %in% c(" Never-married") ~ " NeverMarried",
+  adult$maritalstatus %in% c(" Widowed") ~ "Widowed"
+)
+
 adult$workclass = as.factor(adult$workclass)
 adult$maritalstatus = as.factor(adult$maritalstatus)
 adult$occupation = as.factor(adult$occupation)
 adult$sex = as.factor(adult$sex)
 
-n_obs = nrow(adult)
+# Recode the response to a logical 
+# X50k = 1 (means <=50K, so mark it FALSE), other is TRUE
+adult$X50k = ifelse(adult$X50k == "1", FALSE, TRUE)
 
-# Set seed for reproducibility
-set.seed(1)
+# The code is taking too long to run. We will use a smaller sample of the data
+# Only take 3000 observations from the adult data set 
+adult_sample = adult %>% sample_n(size = 3000)
 
-# Use 20,000 training observations and the rest as test
-n_train = 20000
+n_obs = nrow(adult_sample)
+
+# Use an 70/30 split for the training and test sizes
+n_train = 0.7 * n_obs
 n_test = n_obs - n_train
 
-adult_train = adult[sample(1:n_train, n_train),]
-adult_test = setdiff(adult, adult_train)
-levels(adult_test$X50k) = levels(adult_train$X50k)
+adult_train = adult_sample[1:n_train, ]
+adult_test = adult_sample[(n_train+1):n_obs,]
 
 # Find an appropriate value of C to use in the model 
 # The code takes a long time to run, so pick a smaller set of values
-candidate_Cs = 2^seq(-5, 5, by = 1)
+candidate_Cs = 2^seq(-7, 7, by = 1)
 
-K = 10 # 10-fold CV
+K = 5 # 10-fold CV
 err_matrix = matrix(0, K, length(candidate_Cs))
+
+# Matrix of folds 
+folds = matrix(1:n_train, K)
 
 # Loop across folds, which are rows of the matrix 
 for (k in 1:K) {
   
   # For each fold, create a validation set and a learning set 
   # 20,000 obs in training set / 10 folds = 2000 in each fold 
-  fold_lower_cutoff = 2000*(k-1) + 1
-  fold_upper_cutoff = 2000*k
   
-  valid_fold = adult_train[fold_lower_cutoff:fold_upper_cutoff, ]
-  learn_fold = setdiff(adult_train, valid_fold)
   
-  valid_features = valid_fold[,1:7]
-  valid_Y = valid_fold[,8]
+  valid_ix = folds[k,]
+  learn_ix = setdiff(1:n_train, folds[k,])
   
-  learn_features = learn_fold[,1:7]
-  learn_Y = learn_fold[,8]
+  valid_fold = adult_train[valid_ix,]
+  learn_fold = adult_train[learn_ix,]
   
   # Loop through each value in the candidate set 
   for (i in 1:length(candidate_Cs)) {
@@ -58,9 +74,31 @@ for (k in 1:K) {
     
     # Create predictions on the validation set
     current_preds = predict(current_svm, valid_fold[,1:7])
+    current_preds = as.logical(current_preds)
     
     # Insert the mean test error in the matrix 
-    err_matrix[k, i] = mean(current_preds != valid_Y)
+    err_matrix[k, i] = mean(current_preds != valid_fold$X50k)
     
   }
 }
+
+# Now, get the mean error for each value of C in candidate_C
+mean_errs = apply(err_matrix, 2, mean)
+
+# Get the minimum error
+min_ix = which.min(mean_errs)
+
+C_min = candidate_Cs[min_ix]
+
+# Fit an SVM model with this data set on the entire training set. 
+final_SVM = svm(X50k ~ ., data = adult_train, type = "C-classification",
+                kernel = "linear", cost = C_min)
+
+# Get predictions 
+final_SVM_preds = predict(final_SVM, adult_test)
+final_SVM_preds = as.logical(final_SVM_preds)
+
+# Get the mean test error 
+test_error = mean(final_SVM_preds != adult_test$X50k)
+
+print(sprintf("The final test error for the SVM procedure was %.3f", test_error))
